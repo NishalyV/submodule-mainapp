abbd5ba6379915e159994e9f07c61e970da83f20
NishalyV <nishaly.venkatesan@elpaviation.com>
2025-02-13 16:16:04 +0530

    updated submodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileModule } from 'projects/profile/profile-app/projects/profile/src/public-api';
+import { WorkflowModule } from 'projects/workflow/workflow-app/projects/workflow/src/public-api';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ProfileModule,
+    WorkflowModule
   ],
   providers: [],
   bootstrap: [AppComponent]
